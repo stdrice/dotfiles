567e416e01d044c20d85bf07aea68665a805ef0c
pznguin-kyun <p3nguinkun@proton.me>
2024-11-27 21:09:28 +0700

    major updates

--- README.md
+++ README.md
@@ -1,21 +1,11 @@
 <h1 align="center"><b>✨ pengurice ✨ </b></h1>
 <p align="center">📜 A script that rice your Linux/*nix system.</p>
 
-<p align="center">
-    <a href="https://github.com/pznguin-kyun/pengurice/stargazers"><img src="https://img.shields.io/github/stars/pznguin-kyun/pengurice?style=for-the-badge&color=%23ff8989"></a>
-    <a href="https://github.com/pznguin-kyun/pengurice/issues"><img src="https://img.shields.io/github/issues/pznguin-kyun/pengurice?style=for-the-badge&color=%23a978f6"></a>
-    <a href="https://github.com/pznguin-kyun/pengurice/pulls"><img src="https://img.shields.io/github/issues-pr/pznguin-kyun/pengurice?style=for-the-badge&color=%23a978f6"></a>
-    <a href="https://github.com/pznguin-kyun/pengurice/blob/main/LICENSE"><img src="https://img.shields.io/github/license/pznguin-kyun/pengurice?style=for-the-badge&color=%23f2ff8a"></a>
-</p>
-
 # **Table of Contents**
 - [About ⁉️](#about-%EF%B8%8F)
 - [Starring 🌠](#starring-)
-- [Gallery 📷](#gallery-)
 - [Installation 🛠️](#installation-%EF%B8%8F)
 - [Keybindings ⌨️](#keybindings-%EF%B8%8F)
-- [Credits 👨‍🔧](#credits-)
-- [Support 💝](#support-)
 - [License](#license)
 
 # **About ⁉️**
@@ -23,95 +13,64 @@
 This is pengurice, a script that automatically rice your Linux system. I made this for people who wants a simple, productive and eyecandy Linux desktop.
 
 # **Starring 🌠**
-<img src="https://i.imgur.com/cajwJ7I.png" align="right" width="400px">
+<img src="https://i.imgur.com/1Raaomc.png" align="right" width="400px">
 
-- [pengudotfiles](https://github.com/pznguin-kyun/pengudotfiles): default dotfiles
-- [bspwm ⚽](https://github.com/baskerville/bspwm): window manager
-- [alacritty ❤️](https://alacritty.org/): terminal
+- [sway ⚽](https://github.com/baskerville/bspwm): window manager/compositor
+- [foot ❤️](https://alacritty.org/): terminal
 - [firefox 🦊](https://www.mozilla.org/en-US/firefox/): browser
 - [ranger](https://ranger.github.io/) and [nemo](https://github.com/linuxmint/nemo): file manager
 - [neovim](https://neovim.io): editor
-- [cmus](https://cmus.github.io/): music player
+- [ncmpcpp](https://github.com/ncmpcpp/ncmpcpp): music player
 - [calcurse](https://www.calcurse.org/): calendar
 - [btop](https://github.com/aristocratos/btop): system monitor
 
 and so on!
 
 
-# **Gallery 📷**
-| bloom                                          | catppuccin                                     | dracula                                       |
-| :--------------------------------------------- | :--------------------------------------------- | :-------------------------------------------- |
-| ![bloom](https://i.imgur.com/m0F9ZsP.png)      | ![catppuccin](https://i.imgur.com/x2J3zFt.png) | ![dracula](https://i.imgur.com/tDZ8VmE.png)   |
-| everforest                                     | gruvbox                                        | macaroni                                      |
-| ![everforest](https://i.imgur.com/6SEhR5f.png) | ![gruvbox](https://i.imgur.com/K1GlJk8.png)    | ![macaroni](https://i.imgur.com/6ReXidY.png)  |
-| monochrome                                     | nord                                           | rosepine                                      |
-| ![monochrome](https://i.imgur.com/y3tnFkw.png) | ![nord](https://i.imgur.com/CmhW7Jb.png)       | ![rosepine](https://i.imgur.com/16Y0WZT.png)  |
-| snowy                                          | tokyonight                                     |
-| ![snowy](https://i.imgur.com/YnxsCFS.png)      | ![tokyonight](https://i.imgur.com/DgYvmt4.png) |
-
 # **Installation 🛠️**
 ## Distros
-- Arch Linux and Arch-based distros (EndeavourOS, Artix Linux, Arco Linux, ArchCraft, ...)
-- RedHat-based distros (RHEL, Fedora, Nobara, ...)
-- Debian and Debian-based distros (Ubuntu, Pop! OS, ...)
-- SUSE and openSUSE
-- Void Linux
+Now this script only supports **Arch Linux** and **Arch-based** distros (EndeavourOS, Artix Linux, Arco Linux, ArchCraft, ...) because I'm too lazy to maintain it for other distros.
 
 ## Install with git
 ```
-$ git clone --depth 1 https://github.com/pznguin-kyun/pengurice
+$ git clone --depth 1 https://codeberg.org/pznguin-kyun/pengurice
 $ cd pengurice
 # ./pengurice.sh
 ```
 
 ## Install with curl
 ```
-$ curl -LO https://raw.githubusercontent.com/pznguin-kyun/pengurice/main/pengurice.sh
+$ curl -LO https://codeberg.org/pznguin-kyun/pengurice/raw/branch/main/pengurice.sh
 # bash pengurice.sh
 ```
 
 ## Install manually
-Read [pengudotfiles's README](https://github.com/pznguin-kyun/pengudotfiles) for more information
+- Clone this repo
+- Copy all files in `dotfiles` and paste to your `$HOME` folder
 
 ## Some notes
 - If your system doesn't have `bash`, install it.
-- Tested on latest Arch Linux, Fedora 38, Debian 12, Ubuntu 23.04, OpenSUSE Tumbleweed, latest Void Linux.
 - This script MUST BE run as `root`.
-- If you dont have display manager (or login manager), you can login with xinit.
-```
-startx
-```
 
 # **Keybindings ⌨️**
-These are the basic keybinds. Read through the `bspwm/config/keys` file for more keybinds like increasing gaps and restoring gaps.
-> Note: Super = Windows key
+These are the basic keybinds. Read through the `sway/general/keybindings.conf` file for more keybinds.
+> Note: Mod = Windows key
 
 | Keys                                              | Function                          |
 | :------------------------------------------------ | :-------------------------------- |
-| `Super + Enter`                                   | Launch terminal (alacritty)       |
-| `Super + Q`                                       | Close window                      |
-| `Super + Alt + R`                                 | Restart window manager (bspwm)    |
-| `Super + D`                                       | Open launcher (rofi)              |
-| `Super + BackSpace`                               | Open powermenu                    |
-| `Super + X`                                       | Open menu                         |
-| `Super + Tab`                                     | Open window menu                  |
-| `Super + Shift + C`                               | Open colorscheme changer          |
-| `Super + Delete`                                  | Screenshot menu                   |
-| `Super + Shift + B`                               | Toggle bar (polybar)              |
-| `Super + Shift + P`                               | Toggle compositor (picom)         |
-| `Super + T/Shift + T/F/S/`                        | Tile/Pseudo Tile/Float/Fullscreen |
-| `Super + R`                                       | Rotate window                     |
-| `Super + H/J/K/L or Left/Down/Up/Right`           | Change Focus                      |
-| `Super + Shift + H/J/K/L or Left/Down/Up/Right`   | Move window                       |
-
-# **Credits 👨‍🔧**
-- [Contributors](https://github.com/pznguin-kyun/pengurice/graphs/contributors)
-- Some people on Discord
-
-# **Support 💝**
-If you like my project and would like to support & appreciate it via donation then I'll gladly accept it.
-- [☕Ko-fi](https://ko-fi.com/p3nguinkun)
-- [💳 PayPal](https://paypal.me/p3nguinkun)
+| `Mod + Enter`                                     | Launch terminal (foot)            |
+| `Mod + Q`                                         | Close window                      |
+| `Mod + Shift + C`                                 | Reload config files               |
+| `Mod + D`                                         | Open launcher (rofi)              |
+| `Mod + BackSpace`                                 | Open powermenu                    |
+| `Mod + Delete`                                    | Screenshot menu                   |
+| `Mod + S/W/E`                                     | Stacking/Tabbed/Tile              |
+| `Mod + F`                                         | Toggle Fullscreen                 |
+| `Mod + B/V`                                       | Split Horizontal/Vertical         |
+| `Mod + R`                                         | Resize mode                       |
+| `Mod + Shift + Space`                             | Toggle Floating                   |
+| `Mod + H/J/K/L or Left/Down/Up/Right`             | Change Focus                      |
+| `Mod + Shift + H/J/K/L or Left/Down/Up/Right`     | Move window                       |
 
 # **License**
 This project is licensed under the GNU General Public License v3.0. See the [LICENSE](LICENSE) file for details.
--- pengurice.sh
+++ pengurice.sh
@@ -13,9 +13,10 @@ CNC=$(tput sgr0)
 
 # vars
 date=$(date +%Y%m%d-%H%M%S)
-git_repo="https://github.com/pznguin-kyun/pengudotfiles"
-branch="bspwm"
-main_folder="main"
+git_repo="https://codeberg.org/pznguin-kyun/pengurice"
+branch="main"
+main_folder="dotfiles"
+packages=(bluez bluez-utils brightnessctl btop calcurse dunst firefox foot galculator git gtk-engine-murrine gvfs gvfs-afc gvfs-mtp gvfs-smb imv libnotify lxappearance-gtk3 ly mpc mpd mpv ncmpcpp nemo neofetch neovim networkmanager noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra nwg-displays pamixer papirus-icon-theme pavucontrol pipewire pipewire-pulse polkit-gnome power-profiles-daemon ranger rofi sed slurp sof-firmware sudo sway swaybg swayidle swaylock ttf-roboto-mono-nerd ttf-roboto udisks2 unzip waybar wireplumber xdg-desktop-portal-gtk xdg-desktop-portal-wlr xdg-user-dirs-gtk xorg xorg-drivers xorg-xwayland xsettingsd zathura zathura-pdf-mupdf zip zsh)
 
 # functions
 logo() {
@@ -57,53 +58,30 @@ done
 read -rp "First, type your username here: " username
 }
 
-check_distro(){
-  if command -v pacman &>/dev/null; then
-    distro="nyarch"
-  elif command -v apt &>/dev/null; then
-    distro="debnyan"
-  elif command -v dnf &>/dev/null; then
-    distro="fedornya"
-  elif command -v zypper &>/dev/null; then
-    distro="sus"
-  elif command -v xbps-install &>/dev/null; then
-    distro="vowoid"
-  fi
-}
-
 update(){
     logo "Updating system"
-    case $distro in
-      nyarch) 
-        rm -rf /var/lib/pacman/db.lck
-        case "$(readlink -f /sbin/init)" in
-	      *systemd*)
-          pacman -Sy --noconfirm --needed archlinux-keyring ;;
-        *)
-          pacman --noconfirm --needed -Sy artix-keyring artix-archlinux-support ;;
-        esac
-        pacman -Syyuu --noconfirm ;;
-      debnyan) apt update -y && apt upgrade -y ;;
-      fedornya) dnf update -y ;;
-      sus) zypper dup -y ;;
-      vowoid) xbps-install -Syu ;;
-    esac
+    rm -rf /var/lib/pacman/db.lck
+    case "$(readlink -f /sbin/init)" in
+	    *systemd*)
+        pacman -Sy --noconfirm --needed archlinux-keyring ;;
+      *)
+        pacman --noconfirm --needed -Sy artix-keyring artix-archlinux-support ;;
+      esac
+      pacman -Syyuu --noconfirm ;;
 }
 
 setup_before_install(){
     logo "Setup before install"
-    case "$distro" in
-    nyarch)
-        pacman -Sy --noconfirm --needed curl git base-devel
-        if ! command -v yay &>/dev/null; then
-            rm -rf /tmp/aur 
-            git clone https://aur.archlinux.org/yay-bin.git /tmp/aur
-            chmod 777 /tmp/aur
-            cd /tmp/aur
-            sudo -u "$username" makepkg -si --noconfirm
-            cd
-        fi
-        case "$(readlink -f /sbin/init)" in
+    pacman -Sy --noconfirm --needed curl git base-devel
+    if ! command -v yay &>/dev/null; then
+        rm -rf /tmp/aur 
+        git clone https://aur.archlinux.org/yay-bin.git /tmp/aur
+        chmod 777 /tmp/aur
+        cd /tmp/aur
+        sudo -u "$username" makepkg -si --noconfirm
+        cd
+    fi
+    case "$(readlink -f /sbin/init)" in
 	      *systemd*) return ;;
 	      *)
 		    logo "Enabling Arch Repositories"
@@ -111,53 +89,13 @@ setup_before_install(){
 			  echo -e "[extra]\nInclude = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf
 		    pacman -Sy --noconfirm >/dev/null
 		    pacman-key --populate archlinux >/dev/null ;;
-        esac
-    ;;
-    debnyan)
-        apt install -y curl git ;;
-    vowoid)
-        xbps-install -Sy curl git ;;
-    fedornya)
-	printf "max_parallel_downloads=10\nfastestmirror=1" /etc/dnf/dnf.conf
-        dnf install curl git
-        dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm -y
-        dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm -y 
-        ;;
-    sus) 
-        zypper in -y curl git
-        eval "$(cat /etc/os-release)"
-        if [ -f /etc/os-release ]; then
-            case $ID in
-		        opensuse-tumbleweed)
-			        zypper ar -cfp 90 "https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/" packman
-    			    zypper dup --from packman --allow-vendor-change -y ;;
-    		    opensuse-leap)
-    			    # shellcheck disable=SC2154
-    			    zypper ar -cfp 90 "https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Leap_$releasever/" packman
-    			    zypper dup --from packman --allow-vendor-change -y ;;
-    		    suse)
-    			    zypper ar -cfp 90 "https://ftp.gwdg.de/pub/linux/misc/packman/suse/SLE_$releasever/" packman
-    			    zypper dup --from packman --allow-vendor-change -y ;;
-    		esac
-        fi
-    ;;
-  esac
+    esac
 }
 
 install_pkgs(){
     logo "Installing packages"
-    [ -f /tmp/"$distro".txt ] && rm -rf /tmp/"$distro".txt
-    curl -o /tmp/"$distro".txt https://raw.githubusercontent.com/pznguin-kyun/pengurice/main/packages/"$distro".txt
-    case "$distro" in
-        debnyan) install_command="apt install -y" ;;
-        fedornya) install_command="dnf install -y --allowerasing" ;;
-        nyarch) install_command="pacman -Sy --noconfirm --needed" ;;
-        sus) install_command="zypper in -y" ;;
-        vowoid) install_command="xbps-install -Sy" ;;
-    esac
-    xargs -a /tmp/"$distro".txt $install_command
+    pacman -Sy --noconfirm "${packages[@]}"
 }
-
 
 setup_user(){
     if [ ! "$(id -u "$username" > /dev/null)" ]; then
@@ -181,59 +119,28 @@ clone_dotfiles(){
     git clone --depth=1 "$git_repo" -b "$branch" "$dotfiles_dir"
 }
 
-backup_dotfiles(){
-  backup_folder=/home/"$username"/.RiceBackup
-  logo "Backing-up dotfiles"
-  echo "Backup files will be stored in /home/$username/.RiceBackup" "${BLD}" "${CRE}" "$HOME" "${CNC}"
-  sleep 1
-    if [ ! -d "$backup_folder" ]; then
-    	mkdir -p "$backup_folder"
-    fi
-    [ -d /home/"$username"/.config/bspwm/user ] && mv /home/"$username"/.config/bspwm/user /tmp/bsp_backup
-    for folder in alacritty bspwm dunst gtk-3.0 htop mpd ncmpcpp neofetch newsboat nvim picom pipewire polybar ranger rofi; do
-    	if [ -d /home/"$username"/.config/$folder ]; then
-    		mv /home/"$username"/.config/$folder "$backup_folder/${folder}_$date"
-      fi
-    done
-
-    for file in .xinitrc .fehbg .zshrc .Xresources; do
-      [ -f /home/"$username"/$file ] && mv /home/"$username"/"$file" /home/"$username"/.RiceBackup/"$file"-backup-"$date"
-    done
-}
-
 install_dotfiles(){
     logo "Installing dotfiles.."
     printf "Copying files to respective directories..\n"
     cp -rfT /tmp/dotfiles/"$main_folder"/ /home/"$username"/
-    [ -d /tmp/bsp_backup ] && cp /tmp/bsp_backup /home/"$username"/.config/bspwm/user
     chown -R "$username":"$username" /home/"$username"
     fc-cache -fv
 }
 
 config_smth(){
-    logo "Enable Tap to Click"
-    [ ! -f /etc/X11/xorg.conf.d/40-libinput.conf ] && printf 'Section "InputClass"
-        Identifier "libinput touchpad catchall"
-        MatchIsTouchpad "on"
-        MatchDevicePath "/dev/input/event*"
-        Driver "libinput"
-	# Enable left mouse button by tapping
-	Option "Tapping" "on"
-EndSection' >/etc/X11/xorg.conf.d/40-libinput.conf
-    if command -v pacman &>/dev/null; then
-        logo "Configuring pacman (for what???)"
-        sed -i "s/^#Color$/Color/" /etc/pacman.conf
-        sed -i "s/#NoProgressBar/ILoveCandy/" /etc/pacman.conf
-        sed -i "s/#VerbosePkgLists/VerbosePkgLists/" /etc/pacman.conf
-        sed -i "s/#ParallelDownloads\ =\ 5/ParallelDownloads\ =\ 5/" /etc/pacman.conf
-    fi
+    logo "Configuring pacman (for what???)"
+    sed -i "s/^#Color$/Color/" /etc/pacman.conf
+    sed -i "s/#NoProgressBar/ILoveCandy/" /etc/pacman.conf
+    sed -i "s/#VerbosePkgLists/VerbosePkgLists/" /etc/pacman.conf
+    sed -i "s/#ParallelDownloads\ =\ 5/ParallelDownloads\ =\ 5/" /etc/pacman.conf
 }
 
 enable_services(){
     logo "Enabling services"
     case "$(readlink -f /sbin/init)" in
         *systemd*)
-            systemctl enable NetworkManager
+            systemctl disable display-manager.service
+            systemctl enable NetworkManager ly
             systemctl disable NetworkManager-wait-online.service
             sudo -u "$username" systemctl --user enable pipewire.service
             sudo -u "$username" systemctl --user enable pipewire-pulse.service
@@ -293,13 +200,11 @@ main(){
     root_checking
     bootstraps
     intro
-    check_distro
     update
     setup_before_install
     install_pkgs
     setup_user
     clone_dotfiles
-    backup_dotfiles
     install_dotfiles
     config_smth
     enable_services
