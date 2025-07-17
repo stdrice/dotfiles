e2383a4d8fff29927c06f1489fa8dba5800a9766
stdrice <stdrice@disroot.org>
2025-07-17 18:01:38 +0700

    update

--- dotfiles/.local/share/themes/dark/gtk-3.0/assets/sed.sh
+++ dotfiles/.local/share/themes/dark/gtk-3.0/assets/sed.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sed -i \
          -e 's/#1C1C1E/rgb(0%,0%,0%)/g' \
-         -e 's/#ffffff/rgb(100%,100%,100%)/g' \
+         -e 's/#FFFFFF/rgb(100%,100%,100%)/g' \
     -e 's/#2C2C2E/rgb(50%,0%,0%)/g' \
      -e 's/#0a84ff/rgb(0%,50%,0%)/g' \
      -e 's/#1C1C1E/rgb(50%,0%,50%)/g' \
