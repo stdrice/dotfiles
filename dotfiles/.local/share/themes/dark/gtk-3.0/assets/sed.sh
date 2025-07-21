3ac4f10ec06b5c8fd60deecfb1f8c491b86b901a
stdrice <stdrice@disroot.org>
2025-07-21 11:44:35 +0700

    update

--- dotfiles/.local/share/themes/dark/gtk-3.0/assets/sed.sh
+++ dotfiles/.local/share/themes/dark/gtk-3.0/assets/sed.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 sed -i \
-         -e 's/#1C1C1E/rgb(0%,0%,0%)/g' \
-         -e 's/#FFFFFF/rgb(100%,100%,100%)/g' \
-    -e 's/#2C2C2E/rgb(50%,0%,0%)/g' \
+         -e 's/#1c1c1e/rgb(0%,0%,0%)/g' \
+         -e 's/#ffffff/rgb(100%,100%,100%)/g' \
+    -e 's/#2c2c2e/rgb(50%,0%,0%)/g' \
      -e 's/#0a84ff/rgb(0%,50%,0%)/g' \
-     -e 's/#1C1C1E/rgb(50%,0%,50%)/g' \
-     -e 's/#EBEBF5/rgb(0%,0%,50%)/g' \
+     -e 's/#1c1c1e/rgb(50%,0%,50%)/g' \
+     -e 's/#ffffff/rgb(0%,0%,50%)/g' \
 	"$@"
